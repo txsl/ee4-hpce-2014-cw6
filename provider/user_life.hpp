345a74be7e0ffd234b4e5a79be404e215b9b60aa
Thomas Lim <txl11@ic.ac.uk>
2015-03-16 23:13:12 +0000

    Basic OpenCL implementation for life now working. Output is not currently correct!

--- provider/user_life.hpp
+++ provider/user_life.hpp
@@ -3,8 +3,7 @@
 
 #include "tbb/parallel_for.h"
 
-#define __CL_ENABLE_EXCEPTIONS 
-#include "CL/cl.hpp"
+#include "opencl_frame.hpp"
 
 #include "puzzler/puzzles/life.hpp"
 
@@ -22,53 +21,145 @@ public:
 		       ) const override {
     log->LogVerbose("About to start running iterations (total = %d)", input->steps);
 
-    int K;
-
-    char *v=getenv("HPCE_LIFE_CHUNKSIZE_K");
-    if(v==NULL){
-      K = 16;
-      log->LogInfo("No HPCE_LIFE_CHUNKSIZE_K envrionment variable found");
-        // printf("HPCE_FFT_LOOP_K not set. Using a size of %i instead.\n", chunk_size);
-    }else{
-      K = atoi(v);
-      log->LogInfo("HPCE_LIFE_CHUNKSIZE_K environment variable found");
-        // printf("Using a chunk size of %i (set in the environment variable 'HPCE_FFT_LOOP_K'.\n)", chunk_size);
-    }
-    log->LogInfo("Chunksize set as K=%i", K);
+    char *v;
+    int OPT;
     unsigned n=input->n;
     // std::vector<bool> state=input->state;
     std::vector<int> state(input->state.begin(), input->state.end());
+    std::vector<int> next(n*n);
 
-    log->Log(puzzler::Log_Debug, [&](std::ostream &dst){
-      dst<<"\n";
-      for(unsigned y=0; y<n; y++){
-        for(unsigned x=0; x<n; x++){
-          dst<<(state.at(y*n+x)?'x':' ');
-        }
-        dst<<"\n";
+    // To determine if we are usign TBB or OpenCL
+    v = getenv("HPCE_LIFE_OPT");
+
+    if(v==NULL)
+    {
+      log->LogInfo("No HPCE_LIFE_OPT detected");
+      OPT = 0;
+    }
+    else
+    {
+      log->LogInfo("HPCE_LIFE_OPT detected");
+      OPT = atoi(v);
+    }
+
+    if(OPT)
+    {
+      log->LogInfo("Using TBB implementation");
+
+      int K;
+
+      v=getenv("HPCE_LIFE_CHUNKSIZE_K");
+      if(v==NULL){
+        K = 16;
+        log->LogInfo("No HPCE_LIFE_CHUNKSIZE_K envrionment variable found");
+          // printf("HPCE_FFT_LOOP_K not set. Using a size of %i instead.\n", chunk_size);
+      }else{
+        K = atoi(v);
+        log->LogInfo("HPCE_LIFE_CHUNKSIZE_K environment variable found");
+          // printf("Using a chunk size of %i (set in the environment variable 'HPCE_FFT_LOOP_K'.\n)", chunk_size);
       }
-    });
+      log->LogInfo("Chunksize set as K=%i", K);
 
-    for(unsigned i=0; i<input->steps; i++){
-      log->LogVerbose("Starting iteration %d of %d\n", i, input->steps);
-
-      std::vector<int> next(n*n);
-
-      typedef tbb::blocked_range<unsigned> my_range_t;
-      my_range_t range(0, n, K);
-
-      auto impl = [&](const my_range_t &chunk){
-        for(unsigned x=chunk.begin(); x!=chunk.end(); x++ ){
-          for(unsigned y=0; y<n; y++){
-            next[y*n+x]=int_update(n, state, x, y);
+      log->Log(puzzler::Log_Debug, [&](std::ostream &dst){
+        dst<<"\n";
+        for(unsigned y=0; y<n; y++){
+          for(unsigned x=0; x<n; x++){
+            dst<<(state.at(y*n+x)?'x':' ');
           }
+          dst<<"\n";
         }
-      };
+      });
 
-      // tbb::parallel_for <size_t> (0, n, impl);
-      tbb::parallel_for(range, impl, tbb::simple_partitioner());
+      for(unsigned i=0; i<input->steps; i++){
+        log->LogVerbose("Starting iteration %d of %d\n", i, input->steps);
+
+        typedef tbb::blocked_range<unsigned> my_range_t;
+        my_range_t range(0, n, K);
+
+        auto impl = [&](const my_range_t &chunk){
+          for(unsigned x=chunk.begin(); x!=chunk.end(); x++ ){
+            for(unsigned y=0; y<n; y++){
+              next[y*n+x]=int_update(n, state, x, y);
+            }
+          }
+        };
+
+        // tbb::parallel_for <size_t> (0, n, impl);
+        tbb::parallel_for(range, impl, tbb::simple_partitioner());
+        
+        state = next;
+      }
+    }
+    else
+    {
+      log->LogInfo("Using OpenCL implementation");
+
+      puzzler::OpenclHelper opencl(log);
+      opencl.selectPlatform().selectDevice().makeContext();
+      opencl.build("life_kernel.cl");
+
+      cl::Kernel kernel(opencl.getProgram(), "life_update");
+
+      cl::CommandQueue queue(opencl.getContext(), opencl.getDevice());
+
+      size_t cbBuffer = n*n*sizeof(int);
+      log->LogVerbose("cbBuffer=%i, sizeof(int)=%i", cbBuffer, sizeof(int));
+
+      // cl::Buffer buffN(opencl.getContext(), CL_MEM_READ_ONLY, sizeof(int));
+      cl::Buffer buffCurr(opencl.getContext(), CL_MEM_READ_WRITE, cbBuffer);
+      cl::Buffer buffNext(opencl.getContext(), CL_MEM_READ_WRITE, cbBuffer);
+
+      kernel.setArg(0, n);
+
+      // queue.enqueueWriteBuffer(buffN, CL_TRUE, 0, sizeof(n), n);
+      queue.enqueueWriteBuffer(buffCurr, CL_FALSE, 0, cbBuffer, &state[0], NULL);
+
+      for(int t=0; t<n; t++)
+      {
+        cl::Event evCopiedState;
+        
+        log->LogVerbose("On iteration %i. n=%i", t, n);
+
+        kernel.setArg(1, buffCurr);
+        kernel.setArg(2, buffNext);
+        
+        log->LogVerbose("Arguments set");
+        
+        cl::NDRange offset(0, 0);
+        cl::NDRange globalSize(n, n);
+        cl::NDRange localSize = cl::NullRange;
+
+        // std::vector<cl::Event> kernelDependencies(1, evCopiedState);
+        // cl::Event evExecutedKernel;
+
+        queue.enqueueNDRangeKernel(kernel, offset, globalSize, localSize); //, &kernelDependencies, &evExecutedKernel);
+        
+        log->LogVerbose("Enqueue range set");
+
+        queue.enqueueBarrier();
+
+        log->LogVerbose("Enqueue barrier passed");
+
+        std::swap(buffCurr, buffNext);
+
+        
+        // log->LogInfo("Before copying back");
+        // log->LogInfo("%i in state, %i in next", state.size(), next.size());
+        // log->LogInfo("%i in state, %i in next", sizeof(state), sizeof(next));
+
+        // std::vector<cl::Event> copyBackDependencies(1, evExecutedKernel);
+        // queue.enqueueReadBuffer(buffNext, CL_TRUE, 0, cbBuffer, &next[0], &copyBackDependencies);
+        
+        // log->LogInfo("After copying back");
+        
+        // std::swap(state, next);
+
+      }
       
-      state = next;
+      queue.enqueueReadBuffer(buffNext, CL_TRUE, 0, cbBuffer, &state[0]);
+    
+    }
+
 
       // The weird form of log is so that there is little overhead
       // if logging is disabled
@@ -82,8 +173,6 @@ public:
           dst<<"\n";
         }
       });
-
-    }
 
     log->LogVerbose("Finished steps");
     log->LogVerbose("This is a custom life");
