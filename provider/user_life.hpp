3b4e2b1b1dfc1b39eb6e4a3efb462e7d2b855190
= <dario_dml@hotmail.com>
2015-03-21 18:39:07 +0000

    Cleaned up code s'more

--- provider/user_life.hpp
+++ provider/user_life.hpp
@@ -15,78 +15,68 @@ public:
   {}
 
   virtual void Execute(
-		       puzzler::ILog *log,
-		       const puzzler::LifeInput *input,
-		       puzzler::LifeOutput *output
-		       ) const override {
+    puzzler::ILog *log,
+    const puzzler::LifeInput *input,
+    puzzler::LifeOutput *output
+  ) const override {
     log->LogVerbose("About to start running iterations (total = %d)", input->steps);
 
     char *v;
     int OPT;
-    unsigned n=input->n;
+    unsigned n = input->n;
     // std::vector<bool> state=input->state;
     std::vector<int> state(input->state.begin(), input->state.end());
-    std::vector<int> next(n*n);
+    std::vector<int> next(n * n);
 
     // To determine if we are usign TBB or OpenCL
     v = getenv("HPCE_CL");
 
-    if(v==NULL)
-    {
+    if (v == NULL) {
       log->LogInfo("No HPCE_CL detected");
       OPT = 0;
-    }
-    else
-    {
+    } else {
       log->LogInfo("HPCE_CL detected");
-      OPT = atoi(v);
+      OPT = 1;
     }
 
-    if(!OPT)
-    {
+    if (!OPT) {
       log->LogInfo("Using TBB implementation");
 
-      int K;
-
-      v=getenv("HPCE_CHUNKSIZE_K");
-      if(v==NULL){
-        K = 16;
+      int K = 16;
+      v = getenv("HPCE_CHUNKSIZE_K");
+      if (v == NULL) {
         log->LogInfo("No HPCE_CHUNKSIZE_K envrionment variable found");
-          // printf("HPCE_FFT_LOOP_K not set. Using a size of %i instead.\n", chunk_size);
-      }else{
+      } else {
         K = atoi(v);
-        log->LogInfo("HPCE_CHUNKSIZE_K environment variable found");
-          // printf("Using a chunk size of %i (set in the environment variable 'HPCE_FFT_LOOP_K'.\n)", chunk_size);
+        log->LogInfo("HPCE_CHUNKSIZE_K environment variable found as %i", K);
       }
-      log->LogInfo("Chunksize set as K=%i", K);
 
-      log->Log(puzzler::Log_Debug, [&](std::ostream &dst){
-        dst<<"\n";
-        for(unsigned y=0; y<n; y++){
-          for(unsigned x=0; x<n; x++){
-            dst<<(state.at(y*n+x)?'x':' ');
+      log->Log(puzzler::Log_Debug, [&](std::ostream & dst) {
+        dst << "\n";
+        for (unsigned y = 0; y < n; y++) {
+          for (unsigned x = 0; x < n; x++) {
+            dst << (state.at(y * n + x) ? 'x' : ' ');
           }
-          dst<<"\n";
+          dst << "\n";
         }
       });
 
-      for(unsigned i=0; i<input->steps; i++){
+      for (unsigned i = 0; i < input->steps; i++) {
         log->LogVerbose("Starting iteration %d of %d\n", i, input->steps);
 
         typedef tbb::blocked_range<unsigned> my_range_t;
         my_range_t range(0, n, K);
 
-        auto impl = [&](const my_range_t &chunk){
-          for(unsigned x=chunk.begin(); x!=chunk.end(); x++ ){
-            for(unsigned y=0; y<n; y++){
-              next[y*n+x]=int_update(n, state, x, y);
+        auto impl = [&](const my_range_t &chunk) {
+          for (unsigned x = chunk.begin(); x != chunk.end(); x++ ) {
+            for (unsigned y = 0; y < n; y++) {
+              next[y * n + x] = int_update(n, state, x, y);
             }
           }
         };
 
-        // tbb::parallel_for <size_t> (0, n, impl);
         tbb::parallel_for(range, impl, tbb::simple_partitioner());
-        
+
         state = next;
       }
     }
@@ -94,103 +84,85 @@ public:
     {
       log->LogInfo("Using OpenCL implementation");
 
+      size_t cbBuffer = n * n * sizeof(int);
       puzzler::OpenclHelper opencl(log);
       opencl.selectPlatform().selectDevice().makeContext();
       opencl.build("life_kernel.cl");
 
-      cl::Kernel kernel(opencl.getProgram(), "life_update");
-
       cl::CommandQueue queue(opencl.getContext(), opencl.getDevice());
-
-      size_t cbBuffer = n*n*sizeof(int);
-
       cl::Buffer buffCurr(opencl.getContext(), CL_MEM_READ_WRITE, cbBuffer);
       cl::Buffer buffNext(opencl.getContext(), CL_MEM_READ_WRITE, cbBuffer);
-
-      kernel.setArg(0, n);
-
       queue.enqueueWriteBuffer(buffCurr, CL_FALSE, 0, cbBuffer, &state[0], NULL);
+      
+      cl::Kernel kernel(opencl.getProgram(), "life_update");
+      kernel.setArg(0, n);
+      cl::NDRange offset(0, 0);
+      cl::NDRange globalSize(n, n);
+      cl::NDRange localSize = cl::NullRange;
 
-      for(int t=0; t<n; t++)
-      {
-        cl::Event evCopiedState;
+
         
+      for (int t = 0; t < n; t++) {
         log->LogVerbose("On iteration %i. n=%i", t, n);
 
         kernel.setArg(1, buffCurr);
         kernel.setArg(2, buffNext);
-        
-        log->LogVerbose("Arguments set");
-        
-        cl::NDRange offset(0, 0);
-        cl::NDRange globalSize(n, n);
-        cl::NDRange localSize = cl::NullRange;
 
         queue.enqueueNDRangeKernel(kernel, offset, globalSize, localSize); //, &kernelDependencies, &evExecutedKernel);
-        
-        log->LogVerbose("Enqueue range set");
-
         queue.enqueueBarrier();
 
-        log->LogVerbose("Enqueue barrier passed");
-
         std::swap(buffCurr, buffNext);
-
       }
-      
+
       queue.enqueueReadBuffer(buffCurr, CL_TRUE, 0, cbBuffer, &state[0]);
-    
     }
 
 
-      // The weird form of log is so that there is little overhead
-      // if logging is disabled
-      log->Log(puzzler::Log_Debug, [&](std::ostream &dst){
-        dst<<"\n";
-        for(unsigned y=0; y<n; y++){
-          for(unsigned x=0; x<n; x++){
-            dst<<(next[y*n+x]?'x':' ');
-            // dst<<(orig_next[y*n+x]?'o_x':' ');
-          }
-          dst<<"\n";
+    log->Log(puzzler::Log_Debug, [&](std::ostream & dst) {
+      dst << "\n";
+      for (unsigned y = 0; y < n; y++) {
+        for (unsigned x = 0; x < n; x++) {
+          dst << (next[y * n + x] ? 'x' : ' ');
         }
-      });
+        dst << "\n";
+      }
+    });
 
     log->LogVerbose("Finished steps");
-    log->LogVerbose("This is a custom life");
 
     std::vector<bool> state_tmp(state.begin(), state.end());
 
     output->state = state_tmp;
   }
 
+
   int int_update(int n, const std::vector<int> &curr, int x, int y) const
   {
-    int neighbours=0;
-    for(int dx=-1;dx<=+1;dx++){
-      for(int dy=-1;dy<=+1;dy++){
-        int ox=(n+x+dx)%n; // handle wrap-around
-        int oy=(n+y+dy)%n;
+    int neighbours = 0;
+    for (int dx = -1; dx <= +1; dx++) {
+      for (int dy = -1; dy <= +1; dy++) {
+        int ox = (n + x + dx) % n; // handle wrap-around
+        int oy = (n + y + dy) % n;
 
-        if(curr.at(oy*n+ox) && !(dx==0 && dy==0))
+        if (curr.at(oy * n + ox) && !(dx == 0 && dy == 0))
           neighbours++;
       }
     }
 
-    if(curr[n*y+x]){
+    if (curr[n * y + x]) {
       // alive
-      if(neighbours<2){
+      if (neighbours < 2) {
         return false;
-      }else if(neighbours>3){
+      } else if (neighbours > 3) {
         return false;
-      }else{
+      } else {
         return true;
       }
-    }else{
+    } else {
       // dead
-      if(neighbours==3){
+      if (neighbours == 3) {
         return true;
-      }else{
+      } else {
         return false;
       }
     }
