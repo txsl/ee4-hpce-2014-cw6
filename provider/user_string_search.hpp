0cafd1ce72c65ee4ff0956070bcfebbbff09ca75
Thomas Lim <txl11@ic.ac.uk>
2015-03-21 21:42:12 +0000

    Running each pattern search in a foor loop, now to set it up with TBB

--- provider/user_string_search.hpp
+++ provider/user_string_search.hpp
@@ -20,21 +20,38 @@ public:
 
     std::string data = MakeString(input->stringLength, input->seed);
 
+    int K = 1;
+    v = getenv("HPCE_CHUNKSIZE_K");
+    if (v == NULL) {
+      log->LogInfo("No HPCE_CHUNKSIZE_K envrionment variable found");
+    } else {
+      K = atoi(v);
+      log->LogInfo("HPCE_CHUNKSIZE_K environment variable found as %i", K);
+    } 
+
+    typedef tbb::blocked_range<unsigned> my_range_t;
+    my_range_t range(0, input->patterns.size(), K);
+
     unsigned i = 0;
     while (i < input->stringLength) {
 
+      std::vector <unsigned> lens(input->patterns.size(), 0);
+
+
       for (unsigned p = 0; p < input->patterns.size(); p++) {
-        unsigned len = Matches(data, i, input->patterns[p]);
-        if (len > 0) {
+        lens[p] = Matches(data, i, input->patterns[p]);
+      }
+
+      for (unsigned p = 0; p < input->patterns.size(); p++) {
+        if (lens[p] > 0) {
           log->Log(puzzler::Log_Debug, [&](std::ostream & dst) {
-            dst << "  Found " << input->patterns.at(p) << " at offset " << i << ", match=" << data.substr(i, len);
+            dst << "  Found " << input->patterns.at(p) << " at offset " << i << ", match=" << data.substr(i, lens[p]);
           });
           histogram[p]++;
-          i += len - 1;
+          i += lens[p] - 1;
           break;
         }
       }
-
       ++i;
     }
 
