79a0b5cbf631b01501be9ccdae7b611721371fa4
= <dario_dml@hotmail.com>
2015-03-21 17:36:40 +0000

    fixed user circuit sim

--- provider/user_circuit_sim.hpp
+++ provider/user_circuit_sim.hpp
@@ -9,19 +9,33 @@ class CircuitSimProvider
   : public puzzler::CircuitSimPuzzle
 {
 protected:
-  std::vector<bool> tbb_next(const std::vector<bool> &state, const puzzler::CircuitSimInput *input) const
+
+  int calcSrc(unsigned src, const std::vector<int> &state, const puzzler::CircuitSimInput *input) const
   {
-    std::vector<bool> res(state.size());
+    if (src < input->flipFlopCount) {
+      return state.at(src);
+    } else {
+      unsigned nandSrc = src - input->flipFlopCount;
+      int a = 1 & calcSrc(input->nandGateInputs.at(nandSrc).first, state, input);
+      int b = 1 & calcSrc(input->nandGateInputs.at(nandSrc).second, state, input);
+      return !(a && b);
+    }
+  }
+
+
+  std::vector<int> tbb_next(int chunksize, const std::vector<int> &state, const puzzler::CircuitSimInput *input) const
+  {
+    std::vector<int> res(state.size());
 
     typedef tbb::blocked_range<unsigned> my_range_t;
-    my_range_t range(0,res.size(),512);
+    my_range_t range(0,res.size(),chunkSize);
 
     auto f=[&](const my_range_t &chunk){
       for (unsigned i = chunk.begin(); i < chunk.end(); i++) {
         res[i] = calcSrc(input->flipFlopInputs[i], state, input);
       }
     };
-    tbb::parallel_for(range, f, tbb::simple_partitioner());
+    tbb::parallel_for(range, f);
 
     return res;
   }
@@ -35,18 +49,25 @@ public:
     const puzzler::CircuitSimInput *input, 
     puzzler::CircuitSimOutput *output
   ) const override {
-    // for (unsigned i = 0; i < input->flipFlopInputs.size(); i++) {
-    //   log->LogVerbose("Input Flip Flop, %i, %i", (int)input->flipFlopInputs[i], input->flipFlopCount);
-    // }
+    char *v = getenv("HPCE_CHUNKSIZE_K");
+    int chunkSize = 16;
+    if(v==NULL) {
+      log->LogInfo("No HPCE_CHUNKSIZE_K envrionment variable found. Default to %i", chunkSize);
+    } else {
+      chunkSize = atoi(v);
+      log->LogInfo("HPCE_CHUNKSIZE_K environment variable found and set to %i ", chunkSize);
+    }
+
+
+
     log->LogVerbose("About to start running clock cycles (total = %d)", input->clockCycles);
 
-    std::vector<bool> state = input->inputState;
-
+    std::vector<int> state(input->inputState.begin(), input->inputState.end());
 
     for (unsigned i = 0; i < input->clockCycles ; i++) {
       log->LogVerbose("Starting iteration %d of %d\n", i, input->clockCycles);
 
-      state = tbb_next(state, input);
+      state = tbb_next(chunkSize, state, input);
 
       log->Log(puzzler::Log_Debug, [&](std::ostream & dst) {
         for (unsigned i = 0; i < state.size(); i++) {
@@ -57,7 +78,8 @@ public:
 
     log->LogVerbose("Finished clock cycles");
 
-    output->outputState = state;
+    std::vector<bool> state_tmp(state.begin(), state.end());
+    output->outputState = state_tmp;
   }
 
 };
