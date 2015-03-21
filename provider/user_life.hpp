820871c7953831eab09e49344acb860272560d1b
= <dario_dml@hotmail.com>
2015-03-21 18:49:36 +0000

    removed compiler errors that belong to us

--- provider/user_life.hpp
+++ provider/user_life.hpp
@@ -100,9 +100,7 @@ public:
       cl::NDRange globalSize(n, n);
       cl::NDRange localSize = cl::NullRange;
 
-
-        
-      for (int t = 0; t < n; t++) {
+      for (unsigned t = 0; t < n; t++) {
         log->LogVerbose("On iteration %i. n=%i", t, n);
 
         kernel.setArg(1, buffCurr);
