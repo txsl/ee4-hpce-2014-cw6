e76ca067df100073a001f5787b508fe5986065be
Thomas Lim <txl11@ic.ac.uk>
2015-03-22 18:01:02 +0000

    Install G++

--- aws_bootstrap.sh
+++ aws_bootstrap.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sudo apt-get install g++
+
 wget https://www.threadingbuildingblocks.org/sites/default/files/software_releases/linux/tbb42_20140601oss_lin.tgz --no-check-certificate
 
 tar -xzf tbb42_20140601oss_lin.tgz
